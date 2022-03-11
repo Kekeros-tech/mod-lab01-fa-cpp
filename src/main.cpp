57cce6a2841826fe3cecb1e4e5cf09eaf8701b1b
Kekeros-tech <69265505+Kekeros-tech@users.noreply.github.com>
2022-03-11 15:53:15 +0300

    Main realization

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
-    return 0;
+    const char* string = "This is 1first try to get result";
+    faStr1(string);
+    faStr2(string);
+    faStr3(string);
 }
