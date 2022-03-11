dee424dd7e475f31c5cb7bb3a2351cf8703641b7
Kekeros-tech <69265505+Kekeros-tech@users.noreply.github.com>
2022-03-11 15:45:35 +0300

    First function realization

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,8 +1,36 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <ctype.h>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count = 0;
+    bool inWord = false;
+    int iteration = 0;
+    bool containsNumber = false;
+    while (str[iteration]) {
+        char currentChar = str[iteration];
+        if (inWord == false && currentChar != ' ')
+        {
+            inWord = true;
+            count++;
+        }
+        else if (inWord = true && currentChar == ' ')
+        {
+            if (containsNumber) {
+                containsNumber = false;
+                count--;
+            }
+            inWord = false;
+        }
+        if (inWord = true && isdigit(currentChar)) {
+            containsNumber = true;
+        }
+        iteration++;
+    }
+    if (containsNumber) {
+        return count - 1;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
